714cd20488e0c4ae95c356e0ffd0acec38044725
Avinash Tingre <avinash.tingre@cerence.com>
2021-10-16 17:38:53 +0530

    Generate project for vaccination center service

---
+++ vaccination-center-service/src/main/java/com/avinash/microservice/vaccinationcenterservice/VaccinationCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.avinash.microservice.vaccinationcenterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationCenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationCenterServiceApplication.class, args);
+	}
+
+}
---
+++ vaccination-center-service/src/test/java/com/avinash/microservice/vaccinationcenterservice/VaccinationCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.microservice.vaccinationcenterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCenterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
