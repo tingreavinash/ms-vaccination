211ba187fe8d8fe440c197a91cd208702e9a7d54
Avinash Tingre <avinash.tingre@cerence.com>
2021-10-16 22:04:14 +0530

    Generate project for citizen service

---
+++ citizen-service/src/test/java/com/avinash/microservice/citizenservice/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.microservice.citizenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
