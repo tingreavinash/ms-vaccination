b772cf076e2bbbfe0ecdf467be77280796a6392b
Avinash Tingre <avinash.tingre@cerence.com>
2021-11-07 20:38:29 +0530

    Global exception handling (Extend)

--- citizen-service/src/main/java/io/github/tingreavinash/microservice/citizenservice/advice/MyControllerAdvice.java
+++ citizen-service/src/main/java/io/github/tingreavinash/microservice/citizenservice/advice/MyControllerAdvice.java
@@ -2,13 +2,17 @@ package io.github.tingreavinash.microservice.citizenservice.advice;
 
 import io.github.tingreavinash.microservice.citizenservice.Exception.EmptyInputException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class MyControllerAdvice {
+public class MyControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmptyInputException.class)
     public ResponseEntity<String> handleEmptyInput(EmptyInputException emptyInputException) {
@@ -16,7 +20,13 @@ public class MyControllerAdvice {
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<String> handleEmptyResultDataAccessException(EmptyResultDataAccessException exception){
-        return new ResponseEntity<String>("No matching entity found in DB for given ID, Please look into it - "+exception.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleEmptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        return new ResponseEntity<String>("No matching entity found in DB for given ID, Please look into it - " + exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<Object>("Please change http method type", HttpStatus.METHOD_NOT_ALLOWED);
+
     }
 }
