c1c836d3d3e1a22bab6bc7403511aed048e4ff21
Avinash Tingre <avinash.tingre@cerence.com>
2021-11-21 12:44:26 +0530

    Migrate from Springfox to Openapi

--- citizen-service/README.md
+++ citizen-service/README.md
@@ -1,5 +1,6 @@
-## API Documentation
+# Citizen Service
+### API Documentation
 To see the available API's and their usage, use below document:<br>
-[http://localhost:8082/swagger-ui/][Swagger URL]
+[http://localhost:8082/swagger-ui.html][Swagger URL]
 
-[Swagger URL]: http://localhost:8082/swagger-ui/
+[Swagger URL]: http://localhost:8082/swagger-ui.html
--- citizen-service/src/main/java/io/github/tingreavinash/microservice/citizenservice/config/SwaggerConfig.java
+++ citizen-service/src/main/java/io/github/tingreavinash/microservice/citizenservice/config/SwaggerConfig.java
@@ -1,44 +1,38 @@
 package io.github.tingreavinash.microservice.citizenservice.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collection;
+
 import java.util.Collections;
+import java.util.HashMap;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-    private static final String TITLE = "Citizen Microservice";
-    private static final String DESCRIPTION = "Swagger API documentation for Citizen microservice";
-    private static final String VERSION = "1.0";
-    private static final String TERMSOFSERVICEURL = "";
-    private static final Contact CONTACT = new Contact("Avinash Tingre",
-            "https://tingreavinash.github.io", "tingre.avinash@gmail.com");
-    private static final String LICENSE = "";
-    private static final String LICENSEURL = "";
-
     @Bean
-    public Docket swaggerApiConfig() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("io.github.tingreavinash"))
-                .build()
-                .apiInfo(getApiInfo());
+    public OpenAPI openApiConfig() {
+        return new OpenAPI()
+                .info(getApiInfo())
+                .externalDocs(new ExternalDocumentation()
+                .description("Github Link")
+                .url("https://github.com/tingreavinash/ms-vaccination/tree/master/citizen-service"));
     }
 
-    private ApiInfo getApiInfo() {
-        return new ApiInfo(TITLE, DESCRIPTION, VERSION, TERMSOFSERVICEURL,
-                CONTACT, LICENSE, LICENSEURL, Collections.emptyList());
+    private Info getApiInfo() {
+        return new Info()
+                .title("Citizen Microservice")
+                .description("Swagger API documentation for Citizen microservice")
+                .contact(new Contact()
+                        .name("Avinash Tingre")
+                        .url("https://tingreavinash.github.io")
+                        .email("tingre.avinash@gmail.com"))
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                .version("1.0");
     }
 }
